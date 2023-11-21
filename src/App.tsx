8624f5a118ab2cb01729febcd70054a3c5fc40e9
kngtee <anthonyurah@gmail.com>
2023-11-21 01:24:54 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <main>
+        <Button>Welcome to CLA</Button>
+      </main>
+    </>
+  );
+}
+
+export default App;
