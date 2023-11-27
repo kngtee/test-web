b7bd6028bc41906d3e268206571c69f213fcf7d7
Edesiri <edesiriojakovo@gmail.com>
2023-11-27 02:27:23 -0800

    Menu-navCorrection

--- src/components/MenuNavigation.tsx
+++ src/components/MenuNavigation.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import claLogo from "../assets/images/cla-logo.png";
 import { Button } from "../components/ui/button";
+import { FaChevronDown } from "react-icons/fa6"
+import { RiMenuLine } from "react-icons/ri";
 
 const MenuNavigation = () => {
-  const [submenu, setSubmenu] = useState<any>(false);
-  const [submenu2, setSubmenu2] = useState<any>(false);
+  const [submenu, setSubmenu] = useState(false);
+  const [submenu2, setSubmenu2] = useState(false);
+  const menuRef = useRef(null);
 
   const toggleMenu = () => {
     setSubmenu(!submenu);
@@ -14,12 +17,28 @@ const MenuNavigation = () => {
     setSubmenu2(!submenu2);
   };
 
+  // Close menu when clicking outside
+  useEffect(() => {
+    const handleOutsideClick = (event: { target: any; }) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setSubmenu(false);
+        setSubmenu2(false);
+      }
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [menuRef]);
+
   return (
       <div className="flex flex-row items-center justify-between py-8 bg-[#FBF9F9] px-10">
         <div className="flex justify-center">
-          <img src={claLogo} alt="logo" className=" h-[65.5px]" />
+          <img src={claLogo} alt="logo" className=" h-[40px] md:h-[65.5px]" />
         </div>
-        <div className="flex  rounded-3xl  bg-[white] drop-shadow-xl ">
+        <div className=" md:flex hidden rounded-3xl  bg-[white] drop-shadow-xl " ref={menuRef}>
           <ul className="flex flex-row mt-2 ">
             <li className="p-4 w-[full] cursor-pointer hover:font-bold hover:text-[#900000] flex align-middle">
               Home
@@ -31,14 +50,14 @@ const MenuNavigation = () => {
               onClick={toggleMenu}
               className="p-4 relative cursor-pointer   w-[full] flex hover:font-bold hover:text-[#900000]"
             >
-              Products
+              Products <FaChevronDown  className=" w-[10px] h-[10px] ml-1 mt-2" />
               {submenu && (
-                <ul className="absolute left-0 mt-10 bg-white border rounded shadow-md w-[200px]">
-                  <li className=" py-2 pl-2">Savings and Investment</li>
-                  <li className=" py-2 pl-2">Protection Policies</li>
-                  <li className=" py-2 pl-2">Endowment Policies</li>
-                  <li className=" py-2 pl-2">Annuities</li>
-                  <li className=" py-2 pl-2">Corporate</li>
+                <ul className="absolute left-0 mt-10 hover:font-normal bg-white border rounded   text-gray-700 shadow-md w-[200px]">
+                  <li className=" py-2 pl-2 hover:font-semibold text-sm">Savings and Investment</li>
+                  <li className=" py-2 pl-2 hover:font-semibold text-sm">Protection Policies</li>
+                  <li className=" py-2 pl-2 hover:font-semibold text-sm">Endowment Policies</li>
+                  <li className=" py-2 pl-2 hover:font-semibold text-sm">Annuities</li>
+                  <li className=" py-2 pl-2 hover:font-semibold text-sm">Corporate </li>
                 </ul>
               )}
             </li>
@@ -52,13 +71,13 @@ const MenuNavigation = () => {
               onClick={toggleMenu2}
               className="p-4 relative w-[full] cursor-pointer flex align-middle hover:font-bold hover:text-[#900000]"
             >
-              Resources
+              Resources <FaChevronDown className=" w-[10px] h-[10px] ml-1 mt-2"/>
               {submenu2 && (
-                <ul className="absolute left-0 mt-10 bg-white border rounded shadow-md">
-                  <li className="p-2">FAQs</li>
-                  <li className="p-2">Finacials</li>
-                  <li className="p-2">Downloads</li>
-                  <li className="p-2">Corporates</li>
+                <ul className="absolute left-0 mt-10 bg-white border rounded hover:font-normal text-gray-700 shadow-md">
+                  <li className="p-2 hover:font-semibold text-sm">FAQs</li>
+                  <li className="p-2 hover:font-semibold text-sm">Finacials</li>
+                  <li className="p-2 hover:font-semibold text-sm">Downloads</li>
+                  <li className="p-2 hover:font-semibold text-sm">Corporates</li>
                 </ul>
               )}
             </li>
@@ -68,9 +87,11 @@ const MenuNavigation = () => {
           </ul>
         </div>
 
-        <div className="flex justify-items-center align-middle m-3">
+        <div className="hidden md:flex justify-items-center align-middle m-3">
           <Button>Access Self-Service</Button>
         </div>
+        <RiMenuLine size={20} className="md:hidden"/>
+
       </div>
   );
 };
