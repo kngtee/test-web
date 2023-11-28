f7616f86e3a9f7b1d7a03a1c1cae70d014bf8638
Edesiri <edesiriojakovo@gmail.com>
2023-11-27 21:59:34 -0800

    contact

---
+++ src/components/Contact.tsx
@@ -0,0 +1,14 @@
+import ContactOffices from "./ContactOffices"
+
+
+const Contact = () => {
+  return (
+    <div>
+
+        <ContactOffices />
+      
+    </div>
+  )
+}
+
+export default Contact
