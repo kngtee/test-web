31621045d59e2b87a404c6b9ded697dc8bb60e45
kngtee <anthonyurah@gmail.com>
2024-01-22 01:13:56 +0100

    Home page update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,16 +8,16 @@ const Home = () => {
   return (
     <>
       <GetStarted />
-      <div className="bg-[#3A0000]">
-        <div className="w-full  mx-auto flex p-4 items-center justify-start gap-4">
+      <div className="bg-[#3A0000] h-[94px] items-center justify-center py-[15px] px-[35px]">
+        <div className="w-full mx-auto flex items-center justify-start gap-[29px] py-[15px] px-[35px]">
           <figure className="flex-shrink-0">
             <img src={notice} alt="" className="w-10 h-10" />
           </figure>
           <div className="text-white">
-            <h4 className="text-lg  font-bold">
+            <h4 className="text-[18px] leading-[18.45px]  font-bold">
               Minimum Insurance Capital Requirements
             </h4>
-            <p className="text-xs font-light">
+            <p className="text-[14px] leading-[14.35px] font-light">
               The National Insurance Commission (NAICOM) has substantially
               raised minimum capital requirements for insurance and reinsurance
               firms operating in the country.
